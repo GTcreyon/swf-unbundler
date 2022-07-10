02b100b48df2a34002b8870b6ca2d7eb30ed959f
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-07-10 13:23:51 +0100

    print to stdout

--- unbundle.py
+++ unbundle.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python
 
+import sys
 import argparse
 argparser = argparse.ArgumentParser()
 argparser.add_argument('input', help='input file name')
-argparser.add_argument('-o', '--output', help='output file name')
+argparser.add_argument('-o', '--output', help='output file name; omitting will print to stdout')
 argparser.add_argument('-v', '--verbose', help='print verbose output', action='store_true')
 args = argparser.parse_args()
-
-# give output file name a default suffix if none is specified
-if args.output == None:
-    file_name_out = str(args.input) + '_unbundled.swf'
-    if args.verbose:
-        print('No output specified, defaulting to', file_name_out)
-else:
-    file_name_out = str(args.output)
-    if args.verbose:
-        print('Output file:', file_name_out)
 
 # open input file
 file_in = open(args.input, 'rb')
@@ -38,11 +29,18 @@ if int.from_bytes(file_in.read(4), 'little') == 0xFA123456:
     file_in.seek(-8 - swf_length, 2)
 
     # read the SWF and save it to the output file
-    file_out = open(file_name_out, 'wb')
-    file_out.write(file_in.read(swf_length))
-    file_out.close()
-    print('Successfully unbundled SWF to output file %s! Make sure to check the output file.' % file_name_out)
+    if args.output == None:
+        if args.verbose:
+            print('Printing to stdout')
+        print(file_in.read(swf_length))
+    else:
+        file_out = open(args.output, 'wb')
+        if args.verbose:
+            print('Output file:', args.output)
+        file_out.write(file_in.read(swf_length))
+        print('Successfully unbundled SWF to output file %s! Make sure to check the output file.' % args.output)
+        file_out.close()
 else:
-    print('ERROR: No SWF file detected.')
+    print('ERROR: No SWF file detected.', file=sys.stderr)
 
 file_in.close()
